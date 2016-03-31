38501fa75c02f07225206f4725ed2e63a4d58849
Joel Ward <jmward@gmail.com>
2016-03-31 00:20:23 -0700

    more better

--- loadsaveit.js
+++ loadsaveit.js
@@ -1,14 +1,19 @@
-function loadit(url,callback,errback){
+function loadit(callback,errback,url){
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    xhr.open('GET', url||this.url, true);
     xhr.onload = function() {
-	if(xhr.status===200) callback(xhr.responseText)
-	else                 {if(errback)errback(xhr)}}
-    xhr.send();}
-function saveit(url,text,callback,errback){
+	if(xhr.status===200){
+	    if (callback) callback(xhr.responseText)
+	    else      this.setText(xhr.responseText)
+	}else if (errback) errback(xhr);
+	else console.log("ERROR");
+    }.bind(this);
+    xhr.send()}
+function saveit(callback,errback,url,text){
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
+    xhr.open('POST', url||this.url, true);
     xhr.onload = function() {
         if(xhr.status===200) {if(callback)callback()}
-	else                 {if(errback)errback(xhr)}}
-    xhr.send(text);}
+	else if(errback) errback(xhr)
+	else console.log("ERROR");}
+    xhr.send(text||this.getText())}
