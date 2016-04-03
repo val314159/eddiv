0a7649013ed11ae1316aa9e7e6eb4d411a808891
Joel Ward <jmward@gmail.com>
2016-04-02 21:24:32 -0700

    add python

---
+++ ws.py
@@ -0,0 +1,11 @@
+
+import bottle
+
+@bottle.get('/<path:path>')
+def server_static(path='index.html'):
+    return bottle.static_file(path, root='./')
+
+@bottle.post('/<path:path>')
+def server_static(path='index.html'):
+    open(path,'w').write(bottle.request.body.read())
+    return ['OK']
