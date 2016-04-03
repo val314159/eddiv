98b88452fd361560ea9ce929b2b51267de9a3015
Joel Ward <jmward@gmail.com>
2016-04-02 22:54:13 -0700

    add mousetrap

--- eddiv.js
+++ eddiv.js
@@ -1,3 +1,19 @@
+Mousetrap.bind("escape x",function(e){
+    print("M-x");
+    return false;
+});
+Mousetrap.bind("ctrl+x ctrl+s",function(e){
+    print("C-x C-s");
+    return false;
+});
+Mousetrap.bind("escape :",function(e){
+    print("M-:");
+    return false;
+});
+Mousetrap.bind("escape !",function(e){
+    print("M-!");
+    return false;
+});
 function onkeydown(e){
     var code = e.keyCode || e.charCode;
     if (code===13){
@@ -13,12 +29,18 @@ function onkeydown(e){
 	    selection.collapseToEnd();
 	},0)
 	return false}}
+Mousetrap.bind("enter",onkeydown);
 function eddiv(elt,url){
     if(typeof(elt)==='string') elt=document.getElementById(elt);
-    elt.onkeydown=onkeydown; elt.contentEditable=true; elt.focus();
+    elt.contentEditable=true; elt.focus();
     return Object.create(eddiv.prototype,{elt:{value:elt},url:{value:url}})}
 eddiv.prototype.getText=function( ){ return this.elt.innerHTML }
 eddiv.prototype.setText=function(t){ this.elt.innerHTML = t }
-eddiv.prototype.dump=function(){ console.log(">>",this.getText()) }
+print=function(){
+    document.body.appendChild(document.createTextNode(""+JSON.stringify(arguments)));
+    document.body.appendChild(document.createElement("br"));
+}
+eddiv.prototype.dump=function(){ console.log(">>",this.getText());
+print(this.getText()); }
 eddiv.prototype.load=Lsit.loadit;
 eddiv.prototype.save=Lsit.saveit;
